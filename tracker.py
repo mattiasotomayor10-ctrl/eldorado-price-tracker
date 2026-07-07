1dbf0c6ff951e9acd4f2508386b807214afb8b2c
mattiasotomayor10-ctrl <mattiasotomayor10@gmail.com>
2026-07-07 16:23:31 +0100

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,1 +1,12 @@
+import requests
+from datetime import datetime
 
+url = "https://www.eldorado.gg/it/crunchyroll-premium/t/253?attribute_value_id=premium-mega-fan-12-months"
+
+html = requests.get(
+    url,
+    headers={"User-Agent": "Mozilla/5.0"}
+).text
+
+print("Controllo eseguito:", datetime.now())
+print(html[:500])
